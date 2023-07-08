0348500bb611d33aba330f34a781c1003f0043e4
Adhir <adhmon@lucidity.cloud>
2023-07-08 13:45:52 +0530

    tested, working

--- water_tank_controller/water_tank_controller.ino
+++ water_tank_controller/water_tank_controller.ino
@@ -1,4 +1,5 @@
 #include <WiFi.h>
+#include <HTTPClient.h>
 
 const char *ssid = "HeartBeat_Main";
 const char *password = "connected";
@@ -11,6 +12,10 @@ const int blueLedPin = 2;
 bool is_blue_led_on = false;
 int status_led_count = 4;
 int water_label[] = {95, 95};
+bool relay_state[] = {false, false};
+bool is_enable_channel[] = {true, false};
+
+HTTPClient http;
 
 const int output_led[] = {16, 17, 18, 19, 21, 22, 23, 25, 26, 27};
 const int num_leds = sizeof(output_led) / sizeof(output_led[0]);
@@ -19,42 +24,174 @@ int relay_output[] = {32, 33};
 
 void sync_led()
 {
-	const int num_channels = sizeof(water_label) / sizeof(water_label[0]);
+	uint32_t led_mask = 0; // Initialize the LED mask as all zeros
 
-	for (int channel = 0; channel < num_channels; channel++)
+	print("--------------------------------");
+	if (is_enable_channel[0])
 	{
-		uint32_t led_mask = 0; // Initialize the LED mask as all zeros
-
-		if (water_label[channel] < 10)
+		if (water_label[0] < 15)
 		{
+			Serial.print("_ _ _ _ _");
 			// no light
+			digitalWrite(output_led[0], 0);
+			digitalWrite(output_led[1], 0);
+			digitalWrite(output_led[2], 0);
+			digitalWrite(output_led[3], 0);
+			digitalWrite(output_led[4], 0);
 		}
-		else if (water_label[channel] < 25)
+		else if (water_label[0] < 25)
 		{
-			led_mask = 1; // Turn on LED 1
+			Serial.print("# _ _ _ _");
+			digitalWrite(output_led[0], 1); // Turn on LED 1
+			digitalWrite(output_led[1], 0);
+			digitalWrite(output_led[2], 0);
+			digitalWrite(output_led[3], 0);
+			digitalWrite(output_led[4], 0);
 		}
-		else if (water_label[channel] < 50)
+		else if (water_label[0] < 50)
 		{
-			led_mask = 3; // Turn on LED 1 and LED 2
+			Serial.print("# # _ _ _");
+			digitalWrite(output_led[0], 1);
+			digitalWrite(output_led[1], 1); // Turn on LED 1 and LED 2
+			digitalWrite(output_led[2], 0);
+			digitalWrite(output_led[3], 0);
+			digitalWrite(output_led[4], 0);
 		}
-		else if (water_label[channel] < 75)
+		else if (water_label[0] < 75)
 		{
-			led_mask = 7; // Turn on LED 1, LED 2, and LED 3
+			Serial.print("# # # _ _");
+			digitalWrite(output_led[0], 1);
+			digitalWrite(output_led[1], 1);
+			digitalWrite(output_led[2], 1);
+			// Turn on LED 1, LED 2, and LED 3
+			digitalWrite(output_led[3], 0);
+			digitalWrite(output_led[4], 0);
 		}
-		else if (water_label[channel] < 95)
+		else if (water_label[0] < 95)
 		{
-			led_mask = 15; // Turn on LED 1, LED 2, LED 3, and LED 4
+			Serial.print("# # # # _");
+			digitalWrite(output_led[0], 1);
+			digitalWrite(output_led[1], 1);
+			digitalWrite(output_led[2], 1);
+			digitalWrite(output_led[3], 1);
+			// Turn on LED 1, LED 2, LED 3, and LED 4
+			digitalWrite(output_led[4], 0);
 		}
-		else
+		if (water_label[0] >= 95)
 		{
-			led_mask = 31; // Turn on all LEDs (1 to 5)
+			Serial.print("# # # # #");
+			digitalWrite(output_led[0], 1);
+			digitalWrite(output_led[1], 1);
+			digitalWrite(output_led[2], 1);
+			digitalWrite(output_led[3], 1);
+			digitalWrite(output_led[4], 1);
+			// Turn on all LEDs (1 to 5)
 		}
+		print("");
+	}
 
-		// Set the LED outputs based on the led_mask
-		for (int i = 0; i < sizeof(output_led) / sizeof(output_led[0]); i++)
+	// channel 2:
+	if (is_enable_channel[1])
+	{
+		if (water_label[1] < 10)
 		{
-			digitalWrite(output_led[i], (led_mask >> i) & 1);
+			Serial.print("_ _ _ _ _");
+			// no light
+			digitalWrite(output_led[5], 0);
+			digitalWrite(output_led[6], 0);
+			digitalWrite(output_led[7], 0);
+			digitalWrite(output_led[8], 0);
+			digitalWrite(output_led[9], 0);
 		}
+		else if (water_label[1] < 25)
+		{
+			Serial.print("# _ _ _ _");
+			digitalWrite(output_led[5], 1); // Turn on LED 1
+			digitalWrite(output_led[6], 0);
+			digitalWrite(output_led[7], 0);
+			digitalWrite(output_led[8], 0);
+			digitalWrite(output_led[9], 0);
+		}
+		else if (water_label[1] < 50)
+		{
+			Serial.print("# # _ _ _");
+			digitalWrite(output_led[5], 1);
+			digitalWrite(output_led[6], 1); // Turn on LED 1 and LED 2
+			digitalWrite(output_led[7], 0);
+			digitalWrite(output_led[8], 0);
+			digitalWrite(output_led[9], 0);
+		}
+		else if (water_label[1] < 75)
+		{
+			Serial.print("# # # _ _");
+			digitalWrite(output_led[5], 1);
+			digitalWrite(output_led[6], 1);
+			digitalWrite(output_led[7], 1);
+			// Turn on LED 1, LED 2, and LED 3
+			digitalWrite(output_led[8], 0);
+			digitalWrite(output_led[9], 0);
+		}
+		else if (water_label[1] < 95)
+		{
+			Serial.print("# # # # _");
+			digitalWrite(output_led[5], 1);
+			digitalWrite(output_led[6], 1);
+			digitalWrite(output_led[7], 1);
+			digitalWrite(output_led[8], 1);
+			// Turn on LED 1, LED 2, LED 3, and LED 4
+			digitalWrite(output_led[9], 0);
+		}
+		if (water_label[1] >= 95)
+		{
+			Serial.print("# # # # #");
+			digitalWrite(output_led[5], 1);
+			digitalWrite(output_led[6], 1);
+			digitalWrite(output_led[7], 1);
+			digitalWrite(output_led[8], 1);
+			digitalWrite(output_led[9], 1);
+			// Turn on all LEDs (1 to 5)
+		}
+	}
+}
+
+void parseAndAssignValues(const String &response)
+{
+	// Find the position of the comma in the response
+	int commaPos = response.indexOf(',');
+
+	if (commaPos != -1)
+	{
+		// Extract the substrings before and after the comma
+		String aStr = response.substring(0, commaPos);
+		String bStr = response.substring(commaPos + 1);
+
+		// Convert the substrings to integers
+		water_label[0] = aStr.toInt();
+		water_label[1] = bStr.toInt();
+	}
+	else
+	{
+		Serial.println("Invalid response format: " + response);
+	}
+}
+
+void get_water_status()
+{
+	// Send GET request
+	int httpResponseCode = http.GET();
+	if (httpResponseCode == HTTP_CODE_OK)
+	{
+		// Read the response
+		String response = http.getString();
+		Serial.print("\nreponse: ");
+		Serial.print(response);
+		parseAndAssignValues(response);
+		print("");
+	}
+	else
+	{
+		Serial.print("Error accessing API. Error code: ");
+		Serial.print(httpResponseCode);
 	}
 }
 
@@ -88,13 +225,51 @@ void init_gpio()
 	{
 		pinMode(output_led[i], OUTPUT);
 	}
+	// relay
+	pinMode(relay_output[0], OUTPUT);
+	pinMode(relay_output[1], OUTPUT);
 	print("GPIO initialized for output");
 }
 
+void cron()
+{
+	// channel 1;
+	if (is_enable_channel[0])
+	{
+		if (relay_state[0] && water_label[0] >= 95)
+		{
+			digitalWrite(relay_output[0], 0);
+			relay_state[0] = false;
+			print("relay 1: OFF");
+		}
+		if (!relay_state[0] && water_label[0] < 20)
+		{
+			digitalWrite(relay_output[0], 1);
+			relay_state[0] = true;
+			print("relay 1: ON");
+		}
+	}
+	if (is_enable_channel[1])
+	{
+		if (relay_state[1] && water_label[1] >= 95)
+		{
+			digitalWrite(relay_output[1], 0);
+			relay_state[1] = false;
+			print("relay 2: OFF");
+		}
+		if (!relay_state[1] && water_label[1] < 20)
+		{
+			digitalWrite(relay_output[1], 0);
+			relay_state[1] = true;
+			print("relay 2: ON");
+		}
+	}
+}
 void setup()
 {
 	Serial.begin(115200);
 	print("board on setup mode\n logging service start");
+	http.begin("https://heartbt.xyz/api/test_api");
 
 	init_gpio();
 
@@ -131,9 +306,14 @@ void loop()
 	// your code here
 
 	// TODO: get water lavel status
+	get_water_status();
 
 	// calculation status
 	sync_led();
 
 	// cron
+	cron();
+
+	// delay
+	delay(1000);
 }
